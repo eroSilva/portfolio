e72513463684f711fdf1b95cff53c1fb2ecad1c2
Ero Silva <eronildo.silva@outlook.com>
2018-01-28 13:26:42 -0200

    Inicio do portfolio pessoal

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+
+
+	// URLS
+	var sitePublic     = 'assets';
+	var siteView       = 'views';
+
+
+	// Project configuration.
+	grunt.initConfig({
+		// Compass
+        // @url: http://compass-style.org/
+		compass: {                 
+		    site: {                 
+				options: {
+					sassDir     : sitePublic + '/scss',
+					cssDir      : sitePublic + '/css',
+					watch       : true,
+					sourcemap   : true,
+					environment : 'production'
+				}
+		    }
+		},
+
+		// Browser-Sync
+        // @url: https://www.browsersync.io/
+		browserSync: {
+		    site: {
+		        bsFiles: {
+		            src : [
+		            	sitePublic + '/css/*.css',
+		            	sitePublic + '/css/**/*.css',
+		            	sitePublic + '/js/*.js',
+		            	sitePublic + '/js/**/*.js',
+
+		            	siteView + '/**/*.php',
+		            	siteView + '/**/*.html',
+		            	siteView + '/**/*.phtml',
+		            ]
+		        },
+		        options: {
+		        	online: true,
+		        	watchTask: true,
+		            proxy: "localhost/gosuperinternacional.com.br/front/site"
+		        }
+		    }
+		},
+
+
+		// Uglify
+		// @url: https://www.npmjs.com/package/grunt-contrib-uglify
+		uglify: {
+			options: {
+	        	sourceMap: true
+			},
+			site: {
+				files: [{
+					src: [''],
+					dest: ''
+				}]
+			}
+		}
+	});
+
+
+	// Load plugins
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+	// Define tasks
+	grunt.registerTask('site', ['browserSync:site', 'compass:site']);
+};
